cc546bbe31355d766ecd6fe331c21fffa1dfdc6f
Gu Hua <57578768+loring1@users.noreply.github.com>
2026-08-08 15:33:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,11 +25,6 @@ build_download_url() {
 # 下载地址
 DOWNLOAD_URL=$(build_download_url)
 INSTALL_DIR="/etc/gost"
-COUNTRY=$(curl -s https://ipinfo.io/country)
-if [ "$COUNTRY" = "CN" ]; then
-    # 拼接 URL
-    DOWNLOAD_URL="https://ghfast.top/${DOWNLOAD_URL}"
-fi
 
 
 
